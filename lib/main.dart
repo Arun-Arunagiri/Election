ae83aa8bd60e097c110f924eb47fb9670bd0b751
HP <arunagiri9062@mountzion.ac.in>
2024-07-04 11:17:13 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'dart:async';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/widgets.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'firebase_options.dart';
 
-void main() async{
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -34,58 +32,21 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
   int totalVotes = 0;
-  List<int> votes = [0,0,0,0,0,0,0,0];
-  List<String> names = [];
-  String? downloadURL;
-
+  List<int> votes = List<int>.filled(8, 0);
+  List<String> names = List<String>.filled(8, '');
+  List<String> imgs = ["img1.jpg","img1.jpg","img1.jpg","img1.jpg","img2.jpg","img2.jpg","img2.jpg"];
 
   @override
   void initState() {
     super.initState();
     get_total();
     listenToValueChanges();
-  }
-
-
-
-  Widget tile(String name, int vote) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 25, right: 25, top: 8, bottom: 8),
-      child: Container(
-        height: 100,
-        width: MediaQuery.of(context).size.width / 3.1,
-        decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.all(Radius.circular(20))),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CircleAvatar(
-                radius: 40,
-              ),
-            ),
-            Expanded(child: Text(name, style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold))),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-              child: Column(
-                children: [
-                  Text("VOTES", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12)),
-                  Text("$vote", style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 45)),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
   }
 
   Future<void> get_total() async {
@@ -98,13 +59,11 @@ class _MyHomePageState extends State<MyHomePage> {
           totalVotes = snapshot.value as int;
         });
       }
-      if (snapshot2.exists){
+      if (snapshot2.exists) {
         setState(() {
           names = snapshot2.value.toString().split(',');
-          print(names);
         });
-      }
-      else {
+      } else {
         print('Server Error');
       }
     } catch (e) {
@@ -112,16 +71,11 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-
-
   void listenToValueChanges() {
     final DatabaseReference ref = FirebaseDatabase.instance.ref('votes');
-
     ref.onValue.listen((DatabaseEvent event) {
       if (event.snapshot.exists) {
-        print('Data changed: ${event.snapshot.value}');
-        String temp = event.snapshot.value.toString();
-        List<int> temp_int = temp.split(',').map(int.parse).toList();
+        List<int> temp_int = event.snapshot.value.toString().split(',').map(int.parse).toList();
         setState(() {
           votes = temp_int;
         });
@@ -131,10 +85,47 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Widget tile(String name, int vote, String img) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+      child: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(radius: 30, backgroundImage: AssetImage(img)),
+            ),
+            Expanded(
+              child: Text(
+                name,
+                style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("VOTES", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12)),
+                  Text("$vote", style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 35)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    int voted = votes[0]+votes[1]+votes[2]+votes[3]+votes[4]+votes[5]+votes[6];
+    int voted = votes.reduce((a, b) => a + b);
     int remaining = totalVotes - voted;
     return Scaffold(
       backgroundColor: Colors.black87,
@@ -145,25 +136,25 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  tile(names[0], votes[0]),
-                  tile(names[1], votes[1]),
-                  tile(names[2], votes[2]),
-                  tile(names[3], votes[3]),
-                ],
-              ),
-              Column(
-                children: [
-                  tile(names[4], votes[4]),
-                  tile(names[5], votes[5]),
-                  tile(names[6], votes[6]),
-                ],
-              ),
-            ],
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: List.generate(4, (index) {
+                      return tile(names[index], votes[index], imgs[index]);
+                    }),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: List.generate(3, (index) {
+                      return tile(names[index + 4], votes[index + 4], imgs[index + 4]);
+                    }),
+                  ),
+                ),
+              ],
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 10),
@@ -177,18 +168,19 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50, right: 50),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text("Total Votes", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                        Text("$totalVotes", style: TextStyle(fontSize: 45, color: Colors.blueGrey)),
-                      ],
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text("Total Votes", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                          Text("$totalVotes", style: TextStyle(fontSize: 45, color: Colors.blueGrey)),
+                        ],
+                      ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50, right: 50),
+                  Flexible(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
@@ -197,14 +189,16 @@ class _MyHomePageState extends State<MyHomePage> {
                       ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50, right: 50),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text("Remaining Votes", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                        Text("$remaining", style: TextStyle(fontSize: 45, color: Colors.blue)),
-                      ],
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text("Remaining Votes", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                          Text("$remaining", style: TextStyle(fontSize: 45, color: Colors.blue)),
+                        ],
+                      ),
                     ),
                   ),
                 ],
@@ -216,5 +210,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
